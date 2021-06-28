609499bccde5f618570a71fc5ae47250c4b9ff1c
James <jamesgeraghty@live.ie>
2021-06-28 22:20:19 +0100

    added project #1

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+//import background from "./logo-og.png";
 
 class Projects extends Component{
     constructor(props){
@@ -11,9 +12,34 @@ class Projects extends Component{
 toggleCategories(){
     if (this.state.activeTab === 0){
         return(
-            <div><h3>This is the react Project</h3></div>
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github </Button>
+                 <Button colored> Github </Button>
+                 <Button colored> Github </Button>
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
         )
-    }
+    } else if (this.state.activeTab === 1){
+        return(
+            <div><h3>This is NodeJS </h3></div>
+        )
+    }else if (this.state.activeTab === 2){
+        return(
+            <div><h3>This is MongoDB </h3></div>
+        )
+    }else if (this.state.activeTab === 3){
+        return(
+            <div><h3>This is Devops </h3></div>
+        )
+}
 }
 
 
@@ -25,11 +51,17 @@ toggleCategories(){
                     <Tab>React</Tab>
                     <Tab>NodeJS</Tab>
                     <Tab>MongoDB</Tab>
-                    <Tab>React</Tab>
+                    <Tab>Devops</Tab>
                 </Tabs>
 
                 <section className="project-grid">
-                    {this.toggleCategories()}
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                             <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+
+                    </Grid>
+                    
                 </section>
             </div>
         )
