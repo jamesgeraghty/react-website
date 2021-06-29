0aff5b68834a2ff8b14729e12c1e07ae82215cd5
James <jamesgeraghty@live.ie>
2021-06-29 08:25:08 +0100

    added projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,32 +12,224 @@ class Projects extends Component{
 toggleCategories(){
     if (this.state.activeTab === 0){
         return(
+            <div className="projects-grid">
+
+            {/* // REACT #1 */}
             <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
                 <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
              <CardText>
                  This is a test 
              </CardText>
              <CardActions>
-                 <Button colored> Github </Button>
-                 <Button colored> Github </Button>
-                 <Button colored> Github </Button>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
              </CardActions>
              <CardMenu style={{color: '#ffff'}}>
                  <IconButton name="share" />
                  </CardMenu>
             </Card>
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            </div>
+        
+
+
+
         )
+
+           {/* // Node JS #1 */}
     } else if (this.state.activeTab === 1){
         return(
-            <div><h3>This is NodeJS </h3></div>
+            <div className="projects-grid">
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> Node JS Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            </div>
+        
         )
+
+           {/* // MongoDB #1 */}
     }else if (this.state.activeTab === 2){
         return(
-            <div><h3>This is MongoDB </h3></div>
+            <div className="projects-grid">
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> Mongo DB Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            </div>
+        
         )
+
+           {/* // DEVOPS #1 */}
     }else if (this.state.activeTab === 3){
         return(
-            <div><h3>This is Devops </h3></div>
+            <div className="projects-grid">
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> Devops Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            {/* // Project #1 */}
+            <Card shadow={5} style={{minWidth: '700', margin: 'auto'}}>
+                <CardTitle style= {{color: '#fff', height:'279px', backgroundImage: "url( ./images/react.jpg)"  }}> React Project #1</CardTitle>
+             <CardText>
+                 This is a test 
+             </CardText>
+             <CardActions>
+                 <Button colored> Github Repo </Button>
+                 {/* <Button colored> Github </Button>
+                 <Button colored> Github </Button> */}
+             </CardActions>
+             <CardMenu style={{color: '#ffff'}}>
+                 <IconButton name="share" />
+                 </CardMenu>
+            </Card>
+            </div>
+        
         )
 }
 }
@@ -54,15 +246,15 @@ toggleCategories(){
                     <Tab>Devops</Tab>
                 </Tabs>
 
-                <section className="project-grid">
-                    <Grid className="projects-grid">
+                
+                    <Grid >
                         <Cell col={12}>
                              <div className="content">{this.toggleCategories()}</div>
                         </Cell>
 
                     </Grid>
                     
-                </section>
+                
             </div>
         )
     }
